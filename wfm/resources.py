3261826cbf3b5552d08863f55a48e18e57b78740
David Luo <mail@dsluo.me>
2019-04-09 11:36:40 -0400

    fill out fields for all resources

--- wfm/resources.py
+++ wfm/resources.py
@@ -1,33 +1,108 @@
 from dataclasses import dataclass
+from datetime import datetime
 from typing import List
 
 from .abstract import Resource
 
 
-@dataclass(frozen=True)
+@dataclass(init=False, frozen=True)
 class Item(Resource):
-    url_name: str
     id: str
+    url_name: str
     item_name: str
-    tags: List[str] = None
     thumb: str = None
+    trading_tax: int = None
+    tags: List[str] = None
+    wiki_link: str = None
+    description: str = None
+    ducats: int = None
+    icon: str = None
+    icon_format: str = None
+    sub_icon: str = None
 
-@dataclass(frozen=True)
+    def _args(self):
+        return [self.url_name]
+
+
+@dataclass(init=False, frozen=True)
 class Order(Resource):
-    pass
+    creation_date: datetime
+    visible: bool
+    quantity: int
+    user: 'User'
+    last_update: datetime
+    platinum: int
+    order_type: str
+    region: str
+    platform: str
+    id: str
+    item: Item
 
-@dataclass(frozen=True)
+    def _args(self):
+        return [self.url_name]
+
+
+@dataclass(init=False, frozen=True)
 class Statistics(Resource):
-    pass
+    type: str
+    period: str
+    volume: int
+    min_price: int
+    max_price: int
+    avg_price: float
+    wa_price: float
+    median: int
+    id: str
+    item: Resource
+    open_price: int = None
+    closed_price: int = None
+    moving_avg: float = None
+    order_type: str = None
+    timestamp: datetime = None
+    donch_top: int = None
+    donch_bot: int = None
 
-@dataclass(frozen=True)
-class Profile(Resource):
-    pass
+    def _args(self):
+        return [self.url_name]
 
-@dataclass(frozen=True)
+
+@dataclass(init=False, frozen=True)
+class Achievement(Resource):
+    icon: str
+    description: str
+    name: str
+    type: str
+    exposed: bool
+
+
+@dataclass(init=False, frozen=True)
+class User(Resource):
+    status: str
+    about: str
+    about_raw: str
+    own_profile: bool
+    last_seen: datetime
+    reputation: int
+    region: str
+    achievements: List[Achievement]
+    ingame_name: str
+    background: str
+    platform: str
+    banned: bool
+    id: str
+    avatar: str
+
+
+@dataclass(init=False, frozen=True)
 class Review(Resource):
-    pass
+    user_from: User
+    timestamp: datetime
+    text: str
+    id: str
+    hidden: bool
+    review_type: int
 
-@dataclass(frozen=True)
+
+@dataclass(init=False, frozen=True)
 class Ducat(Resource):
     pass
