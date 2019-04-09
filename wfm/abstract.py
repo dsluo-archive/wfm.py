110b9308e137ce725100de4012c812f9bbe8526f
David Luo <mail@dsluo.me>
2019-04-09 11:35:40 -0400

    type conversions on __init__ _extra attributes provide method for getting args for request on objects without context attach child endpoint methods to resource require to pass parent_resource when building resource

--- wfm/abstract.py
+++ wfm/abstract.py
@@ -1,18 +1,69 @@
 import abc
 import dataclasses
+import functools
+from dataclasses import dataclass
+from datetime import datetime
 from typing import Type, Dict, TYPE_CHECKING, Union, List
 
 if TYPE_CHECKING:
     from .client import Client
 
 
-@dataclasses.dataclass(frozen=True)
+@dataclass(init=False, frozen=True)
 class Resource:
     endpoint: 'Endpoint'
 
-    def __post_init__(self):
-        for name, child in self.endpoint.children.items():
-            setattr(self, name, child.__call__)
+    def __init__(self, endpoint, **attrs):
+        object.__setattr__(self, 'endpoint', endpoint)
+
+        for field in dataclasses.fields(self):
+            if field.name == 'endpoint':
+                continue
+            try:
+                value = attrs.pop(field.name, field.default)
+
+                if value is dataclasses.MISSING:
+                    raise KeyError
+            except KeyError:
+                raise TypeError(f'{field.name} is a required field.')
+
+            try:
+                parsed = datetime.fromisoformat(value)
+                object.__setattr__(self, field.name, parsed)
+                continue
+            except (TypeError, ValueError):
+                pass
+
+            try:
+                if issubclass(field.type, Resource):
+                    if isinstance(value, Resource):
+                        parsed = value
+                    else:
+                        parsed = field.type.endpoint._build_resource(self, value)
+                    object.__setattr__(self, field.name, parsed)
+                    continue
+            except TypeError:
+                pass
+
+            object.__setattr__(self, field.name, value)
+
+        object.__setattr__(self, '_extra', attrs)
+
+    def _args(self):
+        return []
+
+    def _kwargs(self):
+        return {}
+
+    def __getattr__(self, item):
+        if item == 'endpoint':
+            return super(Resource, self).__getattribute__(self, item)
+        try:
+            child = self.endpoint.children[item]
+
+            return functools.partial(child._get, self, *self._args(), **self._kwargs())
+        except KeyError:
+            raise AttributeError
 
 
 class Endpoint(abc.ABC):
@@ -23,13 +74,13 @@ class Endpoint(abc.ABC):
         self.client = client
         self.children = children or {}
 
-    def _get(self, *args, **kwargs) -> Union[resource, List[resource]]:
-        response = self.client._get(self.path.format(*args), **kwargs)
-        return self._build_resource(response)
+    def _get(self, parent_resource, *args, **kwargs) -> Union[resource, List[resource]]:
+        resource = self.client._get(self.path.format(*args), **kwargs)
+        return self._build_resource(parent_resource, resource)
 
     @abc.abstractmethod
-    def _build_resource(self, payload) -> Union[resource, List[resource]]:
+    def _build_resource(self, parent_resource, payload) -> Union[resource, List[resource]]:
         raise NotImplementedError
 
-    def __call__(self, *args, **kwargs) -> Union[resource, List[resource]]:
-        return self._get(*args, **kwargs)
+    def __call__(self, *args, **kwargs) -> Union[Resource, List[Resource]]:
+        return self._get(None, *args, **kwargs)
